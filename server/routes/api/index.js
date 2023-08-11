9d20334d83467a1dfa5e53491cd76fd3ae392365
Jonathan Winter <jonpw2004@outlook.com>
2023-08-10 21:13:47 -0600

    fixed typo

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const drinkRoutes = require("./drinkRoutes");
 const userRoutes = require("./userRoutes");
-const thoughtsRoutes = require("./thoughtsRoutes");
+const thoughtsRoutes = require("./ThoughtsRoutes");
 
 router.use('/drinks', drinkRoutes);
 router.use('/users', userRoutes);
